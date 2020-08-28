3e298771516566f694d61798c202fdf8898eaef1
odair-pedroso <odairpedroso90@gmail.com>
2020-08-28 11:52:01 -0300

    refatoracao servlets

--- WebContent/listaEmpresas.jsp
+++ WebContent/listaEmpresas.jsp
@@ -1,5 +1,5 @@
 
-<%@ page import="java.util.List, br.com.alura.gerenciador.servlet.Empresa"%>
+<%@ page import="java.util.List,br.com.alura.gerenciador.modelo.Empresa"%>
 <%@ page import="org.apache.jasper.tagplugins.jstl.core.ForEach"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
--- src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -1,24 +1,21 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-@WebServlet("/listaEmpresas")
-public class ListaEmpresasServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class ListaEmpresas {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("listando empresas Classe Lista Empresas....");
 
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
